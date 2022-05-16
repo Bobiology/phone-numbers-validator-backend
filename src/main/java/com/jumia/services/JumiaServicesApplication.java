961d00cabd38cdcdbf22a42dbf6f04be1efc695e
Bob Sakawa <bob.sakawa@gmail.com>
2022-05-16 15:04:48 +0300

    jumia service

---
+++ src/main/java/com/jumia/services/JumiaServicesApplication.java
@@ -0,0 +1,13 @@
+package com.jumia.services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JumiaServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JumiaServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jumia/services/JumiaServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jumia.services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JumiaServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
